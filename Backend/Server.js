9fd133424cf25fb9b6b80951a28ad2e8cfa2b6b5
Ravi Gupta <ravigupta4849@gmail.com>
2026-08-06 20:11:28 +0530

    some changes in server.js

--- Backend/Server.js
+++ Backend/Server.js
@@ -8,64 +8,12 @@ import chatRoutes from "./routes/chat.js";
 import authRoutes from "./routes/auth.js";
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 app.use("/api", chatRoutes);
 app.use("/api/auth",authRoutes);
 console.log(process.env.JWT_SECRET);
-// app.post("/chat", async (req, res) => {
-//   const options = {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify({
-//       contents: [
-//         {
-//           parts: [
-//             {
-//               text: req.body.prompt,
-//             },
-//           ],
-//         },
-//       ],
-//     }),
-//   };
-
-//   try {
-//     const { prompt } = req.body;
-//     if (!prompt) {
-//       return res.status(400).json({ error: "Prompt is required" });
-//     }
-//     const response = await fetch(
-//       `https://generativelanguage.googleapis.com/v1beta/models/gemini-3.6-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
-//       options,
-//     );
-//     const data = await response.json();
-
-//     if (!response.ok) {
-//       return res.status(response.status).json(data);
-//     }
-
-//     const answer =
-//       data.candidates?.[0]?.content?.parts?.[0]?.text ||
-//       "No response generated.";
-
-//     // res.json({
-//     //     success: true,
-//     //     reply: answer
-//     // });
-//     res.send(answer);
-//   } catch (err) {
-//     console.error(err);
-
-//     res.status(500).json({
-//       success: false,
-//       message: "Internal Server Error",
-//     });
-//   }
-// });
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
   connectDB();
@@ -79,13 +27,4 @@ const connectDB = async () => {
     console.log("Failed to connect with DB!", error);
   }
 };
-// const ai = new GoogleGenAI({
-//     apiKey: process.env.GEMINI_API_KEY,
-// });
 
-// const response = await ai.models.generateContent({
-//     model: "gemini-3.6-flash",
-//     contents: "give me a joke",
-// });
-
-// console.log(response.text);
